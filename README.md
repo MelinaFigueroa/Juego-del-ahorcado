8b40502e8878de90f628620f95b3973e7c69a2d6
MelinaFigueroa <melina.figueroa.89@gmail.com>
2024-03-09 15:49:03 -0300

    se agrega juego del ahorcado junto a readme actualizado

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-Ｅｎ ｐｒｏｃｅｓｏ．．．
+# Juego del Ahorcado
+
+Este es un simple juego del ahorcado implementado en Python.
+
+## Descripción
+
+En este juego, el jugador intenta adivinar una palabra oculta letra por letra antes de quedarse sin vidas. Si el jugador adivina todas las letras de la palabra antes de perder todas sus vidas, gana el juego.
+
+## Cómo Jugar
+
+1. Ejecuta el script `ahorcado.py` en un entorno de Python.
+2. Introduce tu nombre cuando se te solicite.
+3. Se te mostrará una palabra oculta con un número de guiones bajos que representan las letras de la palabra.
+4. Introduce una letra y presiona Enter para intentar adivinar la palabra.
+5. Si la letra que ingresaste está en la palabra, se revelará su posición. De lo contrario, perderás una vida.
+6. Continúa adivinando letras hasta adivinar la palabra o quedarte sin vidas.
+
+## Requisitos
+
+- Python 3.x
+
+## Ejecución
+
+1. Clona este repositorio o descarga el archivo `ahorcado.py`.
+2. Abre una terminal y navega hasta la ubicación del archivo `ahorcado.py`.
+3. Ejecuta el siguiente comando:
+
+```bash
+python ahorcado.py
+
+
+## Contribuciones
+
+¡Las contribuciones son bienvenidas! Si tienes alguna idea de cómo mejorar este juego, no dudes en hacer un fork del repositorio, implementar los cambios y hacer una solicitud de extracción.
+
+## Autor
+
+Este juego fue creado por Melina Figueroa.
+
--- juegoAhocardo.py
+++ juegoAhocardo.py
@@ -1,17 +1,31 @@
 import time
+
+# Definir la palabra secreta
 palabra_secreta = "ahorcado"
+
+# Solicitar el nombre del jugador
 nombre = input("Bienvenido al juego del ahorcado!!! \n" +
-               "¿Como es tu nombre?\n")
+               "¿Cómo te llamas?\n")
+
+# Mensaje de bienvenida
 print("Hola, " + nombre, "es hora de jugar al ahorcado")
+
+# Inicialización de variables
 time.sleep(1)
 letra_nueva = " "
 vidas = 5
 contador = 0
+
+# Contar la cantidad de letras en la palabra secreta
 for i in palabra_secreta:
     contador += 1
+
+# Mostrar la cantidad de letras de la palabra secreta
 print("*-*-* Palabra de ", +contador, " Letras *-*-*\n" +
       "*-*-* Comienza a adivinar *-*-*",)
 time.sleep(0.5)
+
+# Iniciar el bucle del juego
 while vidas > 0:
     fallas = 0
     for letra in palabra_secreta:
@@ -24,13 +38,17 @@ while vidas > 0:
         print("\nFELICIDADES HAS GANADO, la palabra secreta es: \n", palabra_secreta)
         break
 
+    # Solicitar una letra al jugador
     tu_letra = input("\nIntroduce una letra\n")
     letra_nueva += tu_letra
 
+    # Verificar si la letra ingresada está en la palabra secreta
     if tu_letra not in palabra_secreta:
         vidas -= 1
         print("Intento fallido ")
         print("Te quedan ", +vidas, " vidas")
+
+    # Verificar si el jugador ha perdido todas sus vidas
     if vidas == 0:
         print("Lo siento, has perdido!")
 else:
